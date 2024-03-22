3c1240cafa4d2b08792b937f9eab48c03e3eae62
Crookshanks <752053299@qq.com>
2024-03-23 00:20:35 +0800

    exoplayer add

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,22 +9,17 @@ import android.speech.tts.TextToSpeech.Engine.KEY_PARAM_STREAM
 import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.player.Dispat
 
 
 class MainActivity : AppCompatActivity(){
     private val TAG = "Dispat"
     private var testBt: Button? = null
-    private var dispat:Dispat = Dispat()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_layout)
         testBt = findViewById(R.id.test_btn)
-        dispat.init()
         testBt?.setOnClickListener {
-            val audio = dispat.takeAudioPack()
-            Log.i(TAG, "audio: $audio")
-            dispat.takeVideoPack()
+
         }
     }
 
